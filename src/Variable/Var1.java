e69c231eb2f3d53e4551f6d56ddd414de0906fec
Eonseo <139443553+sseeool@users.noreply.github.com>
2024-10-03 12:49:41 +0900

    add: Section3.1: Start Variable

---
+++ src/Variable/Var1.java
@@ -0,0 +1,20 @@
+package Variable;
+
+public class Var1 {
+    public static void main(String[] args) { //  psvm
+        System.out.println(10); // sout
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
+
+/*
+패키지: 지금 단계에서는 자바 파일을 구분하기 위한 폴더로 이해하면 됨
+관례상 대문자로 시작
+
+Variable 이라는 패키지를 만들었다면,
+해당 패키지에 들어가는 자바 파일 첫줄에 package Variable; 과 같이
+소속된 패키지를 선언해주어야 함
+
+자바 파일이 위치하는 패키지와 package Variable 선언 위치가 같아야 함
+*/
---
+++ src/Variable/Var2.java
@@ -0,0 +1,17 @@
+package Variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 20; // 변수 초기화
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
+
+/*
+변수: 값을 보관해두고 필요할 때 마다 값을 꺼내서 읽을 수 있는 저장소
+이름 그대로 변할 수 있다는 뜻
+*/
